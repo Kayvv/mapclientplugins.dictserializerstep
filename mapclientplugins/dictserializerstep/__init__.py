9933e9e6fdbe7f91b7d6eb5ee9df3ee49d7c8d71
hsorby <h.sorby@auckland.ac.nz>
2021-11-03 12:40:27 +1300

    Set location metadata for step.

--- mapclientplugins/dictserializerstep/__init__.py
+++ mapclientplugins/dictserializerstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Dict Serializer'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.dictserializerstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.dictserializerstep import step
